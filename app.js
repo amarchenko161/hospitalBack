e25d6f2f618e09cfa952366ab2daf8990031240b
amarchenko161 <amarchenko.exceedteam@gmail.com>
2021-11-18 14:47:32 +0300

    add login user

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const Schema = mongoose.Schema;
+const { secret } = require("./config");
 
 const userSchema = new Schema({
-  login: String,
-  password: String,
+  login: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
 });
 
 app.use(cors());
@@ -17,13 +20,42 @@ const uri =
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const User = mongoose.model("hospitalUser", userSchema);
 
+const generateAccessToken = (id) => {
+  const payload = { id };
+  return jwt.sign(payload, secret, { expiresIn: "24h" });
+};
+
+app.post("/singin", (req, res) => {
+  if (req.body.hasOwnProperty("login") && req.body.hasOwnProperty("password")) {
+    User.findOne({ login: req.body.login }).then((result) => {
+      if (result) {
+        const checkPassword = bcrypt.compareSync(
+          req.body.password,
+          result.password
+        );
+        if (!checkPassword) {
+          res.status(404).send("error password");
+        } else {
+          const token = generateAccessToken(result._id);
+          res.send(token);
+        }
+      } else {
+        res.status(404).send("error login");
+      }
+    });
+  } else {
+    res.status(404).send("Error");
+  }
+});
+
 app.post("/createUser", (req, res) => {
   if (req.body.hasOwnProperty("login") && req.body.hasOwnProperty("password")) {
+    const hashPassword = bcrypt.hashSync(req.body.password, 6);
+    const user = new User({ login: req.body.login, password: hashPassword });
     User.findOne({ login: req.body.login }).then((result) => {
       if (result) {
         res.status(404).send("err");
       } else {
-        const user = new User(req.body);
         user.save().then((result) => {
           res.send({ data: result });
         });
